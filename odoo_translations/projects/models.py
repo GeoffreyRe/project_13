24860915f6db51a2ff4f12436684da7ac9981c17
Geoffrey Remacle <geoffreyremacle@outlook.fr>
2020-09-03 12:39:29 +0200

    première vue test + création modèle Role

--- odoo_translations/projects/models.py
+++ odoo_translations/projects/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+class Role(models.Model):
+    """
+    This model consists of the role of an user when he works on a project.
+    The role has an important part when the user wants to make some actions
+    """
+    DEVELOPER = "DEV"
+    TRANSLATOR = "TRA"
+    NAME_CHOICES = [
+        (DEVELOPER, "Développeur"),
+        (TRANSLATOR, "Traducteur")
+    ]
+    # define de name of the role.
+    name = models.CharField("Nom du rôle",
+                            max_length=3,
+                            choices=NAME_CHOICES,
+                            unique=True)
+
+    def save(self, *args, **kwargs):
+        """
+        We overwrite the save method to run validators before saving.
+        """
+        self.full_clean()
+        super().save(*args, **kwargs)
+        
--- odoo_translations/projects/views.py
+++ odoo_translations/projects/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def first_view(request):
+    text_response = "Ceci est la première vue du projet !"
+    return HttpResponse(text_response)
