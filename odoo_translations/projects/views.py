62d67be8cdad7eb93ff447c421ffba1d942a8ab3
Geoffrey Remacle <geoffreyremacle@outlook.fr>
2020-09-11 17:19:53 +0200

    ajout du template bootstrap + page d'accueil

--- odoo_translations/projects/views.py
+++ odoo_translations/projects/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def first_view(request):
-    text_response = "Ceci est la première vue du projet !"
-    return HttpResponse(text_response)
+    
+    return render(request, "home.html")
