b5212c42c5a8b0ade86a962e7f47bab7537e56c6
Geoffrey Remacle <geoffreyremacle@outlook.fr>
2021-02-13 19:38:54 +0100

    correction double url

--- odoo_translations/translations/urls/urls.py
+++ odoo_translations/translations/urls/urls.py
@@ -4,6 +4,5 @@ from translations import views
 urlpatterns = [
     path('<str:instance_type>', views.instance_translation_list, name="instances_list"),
     path('<str:instance_type>/<int:instance_id>', views.instance_translations, name="instance_translations"),
-    path('<str:instance_type>/all', views.all_instance_translations, name="all_instance_translations"),
     path('<str:instance_type>/all', views.all_instance_translations, name="all_instance_translations")
 ]
