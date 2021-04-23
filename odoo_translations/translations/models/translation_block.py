ef842347a8f5cde1d7fbea09243aa26aa09dcef0
Geoffrey Remacle <geoffreyremacle@outlook.fr>
2021-04-23 17:04:04 +0200

    suite mise à jour code pep 8 translations

--- odoo_translations/translations/models/translation_block.py
+++ odoo_translations/translations/models/translation_block.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.apps import apps
-from translations.exceptions import NoTranslationFoundInFileError, NoOdooTranslationFileHeader, TranslationBlockStructureNotGoodError
 from translations.managers import CustomTranslationBlockManager
 import re
 
 
 class TranslationBlock(models.Model):
-    file = models.ForeignKey('translations.TranslationFile',
-                            on_delete=models.CASCADE,
-                            null=False,
-                            related_name="translation_blocks")
+    file = models.ForeignKey(
+        'translations.TranslationFile',
+        on_delete=models.CASCADE,
+        null=False,
+        related_name="translation_blocks")
     raw_text = models.TextField(null=False)
     is_header = models.BooleanField(default=False)
     original_text = models.TextField(null=True, blank=True)
@@ -18,7 +18,6 @@ class TranslationBlock(models.Model):
 
     def __str__(self):
         return "block de traduction numéro {} du fichier {}".format(self.id, self.file.name)
-
 
     @staticmethod
     def find_lines_infos(translation_lines):
@@ -45,36 +44,37 @@ class TranslationBlock(models.Model):
                         "name": line[8:]
                     }
                     line_infos["line_type"] = "code"
-                
+
                 else:
                     line_infos["instance"] = False
 
             elif len(line_parts) == 2:
-                if line_parts[0] in ["#: model:ir.model",
-                "#: model:ir.model.fields", "#: model:ir.ui.menu",
-                "#: model:ir.actions.act_window", "#: model:ir.ui.view"]:
+                line_names_list = [
+                    "#: model:ir.model",
+                    "#: model:ir.model.fields",
+                    "#: model:ir.ui.menu",
+                    "#: model:ir.actions.act_window",
+                    "#: model:ir.ui.view"]
+                if line_parts[0] in line_names_list:
                     line_instance = line_parts[1].split(':')
                     if len(line_instance) == 2:
-                    
+
                         line_infos["instance"] = {
-                        "type": line_parts[0][9:],
-                        "name": line_instance[1]
+                            "type": line_parts[0][9:],
+                            "name": line_instance[1]
                         }
                         line_infos["line_type"] = line_instance[0]
                     else:
                         line_infos["instance"] = False
 
-
-                        
                 else:
                     line_infos["instance"] = False
             else:
                 line_infos["instance"] = False
-            
-            lines_infos.append(line_infos)
-        
-        return lines_infos
 
+            lines_infos.append(line_infos)
+
+        return lines_infos
 
     @staticmethod
     def check_errors_content(data, line_pos, is_header=False, file_name=False):
@@ -84,33 +84,41 @@ class TranslationBlock(models.Model):
         line ==>
         This method will check if there are errors in block structure
         if yes: --> return error message
-        if no: --> return False 
+        if no: --> return False
         """
         begin_line = line_pos
         if is_header:
             if data[0] != "# Translation of Odoo Server.":
-                return (True, "Erreur lors de l'analyse du fichier {}: ligne {} --- le premier bloc n'est pas le header".format(file_name, begin_line))
+                return (
+                    True,
+                    "Erreur lors de l'analyse du fichier {}: ligne {} --- "
+                    "le premier bloc n'est pas le header".format(file_name, begin_line))
             else:
                 return (False,)
 
         # if it is a simple translation block
         if len(data) < 4:
-                # if length of block is below 4, then it means that the block structure is not good.
-                # Indeed, we need, for a block at least 4 lines : one for the module specification,
-                # at least one for a instance to translate (translation line), and 2 for the sentence to translate
-                # msgstr and msgid
-                return (True,
-            "fichier {}, ligne {} : la structure du bloc n'est pas conforme -> taille du bloc inférieur à 4 lignes".format(file_name, line_pos))
-        
+            # if length of block is below 4, then it means that the block structure is not good.
+            # Indeed, we need, for a block at least 4 lines : one for the module specification,
+            # at least one for a instance to translate (translation line),
+            # and 2 for the sentence to translate
+            # msgstr and msgid
+            return (
+                True,
+                "fichier {}, ligne {} : la structure du bloc n'est pas conforme"
+                " -> taille du bloc inférieur à 4 lignes".format(file_name, line_pos))
+
         # the first line of block should be the module specification line
         supported_lines = []
         module_spec_line = data[0]
         if not module_spec_line.startswith("#. module:"):
-            return (True,
-            "fichier {}, ligne : {} --> ligne de specification du module attendue mais non trouvée".format(file_name, line_pos))
+            return (
+                True,
+                "fichier {}, ligne : {} --> ligne de specification "
+                "du module attendue mais non trouvée".format(file_name, line_pos))
         # we extract module name from module specification line
         supported_lines.append(module_spec_line.strip())
-        
+
         # now we have to find msgid and msgstr
         msgid_text = None
         msgstr_text = None
@@ -134,19 +142,21 @@ class TranslationBlock(models.Model):
                     line_to_append = next_line.strip()
                     if re.fullmatch('^".*"$', line_to_append):
                         line_to_append = line_to_append[1:-1]
-                    
+
                     msgid_text.append(line_to_append)
                     i += 1
                     if (pos + i) < len(data[1:]):
                         next_line = data[1:][pos + i]
                     else:
                         next_line = ""
-                        
+
             elif line.startswith('msgstr "'):
                 if msgid_text is None:
                     # if we found a msgstr before msgid, this is not a good structure
-                    return (True,
-                    "Erreur fichier {}, ligne {}: msgstr trouvé avant msgid".format(file_name, line_pos))
+                    return (
+                        True,
+                        "Erreur fichier {}, ligne {}:"
+                        " msgstr trouvé avant msgid".format(file_name, line_pos))
 
                 msgstr_text = []
                 line_to_append = line[6:].strip()
@@ -169,38 +179,45 @@ class TranslationBlock(models.Model):
                         next_line = data[1:][pos + i]
                     else:
                         next_line = ""
-                    
 
-            elif line.startswith('#: model:') or line.startswith("#: code:") or line.startswith('#, python-format') or line.startswith('#: '):
+            elif line.startswith('#: model:') or line.startswith("#: code:") or\
+                    line.startswith('#, python-format') or line.startswith('#: '):
                 supported_lines.append(line.strip())
 
-            
             elif line.startswith('"'):
                 # if the line begins with this part, then it means it is a part of msgid or msgstr
                 pass
 
             else:
                 supported_lines.append(line.strip())
-                #return (True, 'Erreur :Fichier {}, ligne {} non reconnue'.format(file_name, line_pos))
-            
-        
+                # return (True, 'Erreur :Fichier {}, ligne {}
+                # non reconnue'.format(file_name, line_pos))
+
         if (not msgid_text) or (not msgstr_text):
             # if we found partially (or not at all) informations about msgid and msgstr
             # we raise error
-            return (True,
-            "Erreur fichier {}, bloc ligne {} : msgstr et/ou msgid non trouvé dans le bloc".format(file_name, begin_line))
+            return (
+                True,
+                "Erreur fichier {}, bloc ligne {} : msgstr et/ou"
+                " msgid non trouvé dans le bloc".format(
+                    file_name,
+                    begin_line
+                )
+                )
 
         # now we will find informations about lines (line type, instance to translate, etc...)
         TranslationBlock = apps.get_model('translations', 'TranslationBlock')
         supported_lines_parsed = TranslationBlock.find_lines_infos(supported_lines)
-        
-        return (False,
-        {"block": "\n".join(data),
-        "msgid": "\n".join(msgid_text) if isinstance(msgid_text, list) else msgid_text,
-        "msgstr": "\n".join(msgstr_text) if isinstance(msgstr_text, list) else msgstr_text,
-        "supported_lines": supported_lines_parsed
-        })
-    
+
+        return (
+            False,
+            {
+                "block": "\n".join(data),
+                "msgid": "\n".join(msgid_text) if isinstance(msgid_text, list) else msgid_text,
+                "msgstr": "\n".join(msgstr_text) if isinstance(msgstr_text, list) else msgstr_text,
+                "supported_lines": supported_lines_parsed
+            })
+
     class Meta:
         # rename table created by django in db
-        db_table="translation_block"
+        db_table = "translation_block"
--- odoo_translations/translations/models/translation_line.py
+++ odoo_translations/translations/models/translation_line.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.apps import apps
-from translations.exceptions import NoTranslationFoundInFileError, NoOdooTranslationFileHeader, TranslationBlockStructureNotGoodError
 from translations.exceptions import FileParsingError
 from django.core.exceptions import ObjectDoesNotExist
 
+
 class TranslationLine(models.Model):
     block = models.ForeignKey('translations.TranslationBlock',
-                            on_delete=models.CASCADE,
-                            related_name="translation_lines")
+                              on_delete=models.CASCADE,
+                              related_name="translation_lines")
     in_translation_file = models.BooleanField(null=False)
     in_template_file = models.BooleanField(null=True)
     line_type = models.ForeignKey(
@@ -22,21 +22,21 @@ class TranslationLine(models.Model):
         blank=False,
         null=False
         )
-    
+
     def __str__(self):
         return "ligne de traduction numéro {}".format(self.id)
 
     def find_or_create_instance(self, name, instance_type, instance_line_type):
         Instance = apps.get_model('translations', 'Instance')
         instance = Instance.objects.filter(name=name,
-                                            instance_type=instance_type,
-                                            project=self.block.file.project)       
+                                           instance_type=instance_type,
+                                           project=self.block.file.project)
         if len(instance) == 1:
             instance = instance[0]
         else:
             instance = Instance.objects.create(
                 name=name,
-                instance_type= instance_type,
+                instance_type=instance_type,
                 project=self.block.file.project
             )
 
@@ -55,25 +55,35 @@ class TranslationLine(models.Model):
             instance_type = InstanceType.objects.filter(
                 name=instance_type_name)
             if len(instance_type) != 1:
+                error_s = 'fichier {}, bloc ligne {} : {} --- problème avec le type d\'instance'
                 raise FileParsingError(
-            'fichier {}, bloc ligne {} : {} --- problème avec le type d\'instance'.format((file_name, block_position, data.get('line', ''))))
+                    error_s.format((file_name, block_position, data.get('line', ''))))
             # then if instance type exists, we will check if an instance
             # of this type already exists, else we create it
             instance_name = data['instance']['name']
-            line_type_value =  data['line_type']
-            
+            line_type_value = data['line_type']
+
             try:
                 instance_line_type = LineType.objects.get(name=line_type_value)
             except ObjectDoesNotExist:
-                error_string = 'Fichier: {}, bloc ligne {} : "{}" '.format(file_name, block_position, data.get('line', ''))
+                error_string = 'Fichier: {}, bloc ligne {} : "{}" '.format(
+                    file_name,
+                    block_position,
+                    data.get('line', '')
+                    )
                 error_string += '\n"{}" non reconnu'.format(line_type_value)
                 raise FileParsingError(error_string)
-            
-            instance_name_parts = instance_name.split('.') if instance_type_name != "code" else instance_name.split(':')
+
+            instance_name_parts = instance_name.split('.') if instance_type_name != "code" \
+                else instance_name.split(':')
             if len(instance_name_parts) != 2 and instance_type_name != 'module':
                 # we have to get module name and instance name
                 # but for a module line, it doesn't matter
-                error_string = 'Fichier: {}, bloc ligne {} : "{}" '.format(file_name, block_position, data.get('line', ''))
+                error_string = 'Fichier: {}, bloc ligne {} : "{}" '.format(
+                    file_name,
+                    block_position,
+                    data.get('line', '')
+                    )
                 error_string += '\nNom du module et/ou de l\'instance manquant(e)'
                 raise FileParsingError(error_string)
 
@@ -85,35 +95,41 @@ class TranslationLine(models.Model):
             menu_instance_type = InstanceType.objects.get(name='ir.ui.menu')
             code_instance_type = InstanceType.objects.get(name='code')
             code_pos_instance_type = InstanceType.objects.get(name='code.position')
-            
+
             if instance_type_name == 'ir.model.fields':
                 if not instance_name_parts[1].startswith('field_'):
-                    error_string = 'Fichier: {}, bloc ligne {} : "{}" '.format(file_name, block_position, data.get('line', ''))
-                    error_string += '\n"{}" ne commençant pas par "field_"'.format(instance_name_parts[1])
+                    error_string = 'Fichier: {}, bloc ligne {} : "{}" '.format(
+                        file_name, block_position, data.get('line', '')
+                        )
+                    error_string += '\n"{}" ne commençant pas par "field_"'.format(
+                        instance_name_parts[1]
+                        )
                     raise FileParsingError(error_string)
                 model_field_name = instance_name_parts[1][6:]
                 # now we have to find the model inside the field
                 found = False
                 model_field_parts = model_field_name.split('_')
-                module_instance = Instance.objects.filter(name=instance_name_parts[0],
-                                                        instance_type=module_instance_type,
-                                                        project=self.block.file.project)
+                Instance.objects.filter(name=instance_name_parts[0],
+                                        instance_type=module_instance_type,
+                                        project=self.block.file.project)
 
-                while found == False and len(model_field_parts) >= 1:
-                    
+                while found is False and len(model_field_parts) >= 1:
+
                     model_instance = Instance.objects.filter(name="_".join(model_field_parts),
-                                            instance_type=model_instance_type,
-                                            project=self.block.file.project)
-                    
+                                                             instance_type=model_instance_type,
+                                                             project=self.block.file.project)
+
                     if len(model_instance) == 1:
                         model_instance = model_instance[0]
                         found = True
                     else:
                         model_field_parts.pop()
-                
-                if found == False:
+
+                if found is False:
                     print(model_field_name)
-                    error_string = 'Fichier: {}, bloc ligne {} : "{}" '.format(file_name, block_position, data.get('line', ''))
+                    error_string = 'Fichier: {}, bloc ligne {} : "{}" '.format(
+                        file_name, block_position, data.get('line', '')
+                        )
                     error_string += '\n modèle "{}" introuvable'.format(model_field_name)
                     raise FileParsingError(error_string)
 
@@ -122,9 +138,9 @@ class TranslationLine(models.Model):
                     field_name = field_name[1:]
 
                 field_instance = Instance.objects.filter(name=field_name,
-                                            instance_type=field_instance_type,
-                                            parent=model_instance,
-                                            project=self.block.file.project)
+                                                         instance_type=field_instance_type,
+                                                         parent=model_instance,
+                                                         project=self.block.file.project)
                 if len(field_instance) == 1:
                     field_instance = field_instance[0]
                 else:
@@ -136,40 +152,61 @@ class TranslationLine(models.Model):
                     )
                 self.instance = field_instance
                 self.line_type = instance_line_type
-            
+
             elif instance_type_name == "ir.model":
                 if not instance_name_parts[1].startswith('model_'):
-                    error_string = 'Fichier: {}, bloc ligne {} : "{}" '.format(file_name, block_position, data.get('line', ''))
-                    error_string += '\n"{}" ne commençant pas par "model_"'.format(instance_name_parts[1])
+                    error_string = 'Fichier: {}, bloc ligne {} : "{}" '.format(
+                        file_name, block_position, data.get('line', '')
+                        )
+                    error_string += '\n"{}" ne commençant pas par "model_"'.format(
+                        instance_name_parts[1]
+                        )
                     raise FileParsingError(error_string)
 
-                self.find_or_create_instance(instance_name_parts[1][6:], model_instance_type, instance_line_type)
-            
+                self.find_or_create_instance(
+                    instance_name_parts[1][6:], model_instance_type, instance_line_type
+                    )
+
             elif instance_type_name == "module":
-                self.find_or_create_instance(instance_name_parts[0], module_instance_type, instance_line_type)
-            
+                self.find_or_create_instance(
+                    instance_name_parts[0],
+                    module_instance_type,
+                    instance_line_type
+                    )
+
             elif instance_type_name == "ir.ui.view":
-                self.find_or_create_instance(instance_name_parts[1], view_instance_type, instance_line_type)
-            
+                self.find_or_create_instance(
+                    instance_name_parts[1],
+                    view_instance_type,
+                    instance_line_type
+                    )
+
             elif instance_type_name == "ir.actions.act_window":
-                self.find_or_create_instance(instance_name_parts[1], actwindow_instance_type, instance_line_type)
-                
-            
+                self.find_or_create_instance(
+                    instance_name_parts[1],
+                    actwindow_instance_type,
+                    instance_line_type
+                    )
+
             elif instance_type_name == "ir.ui.menu":
-                self.find_or_create_instance(instance_name_parts[1], menu_instance_type, instance_line_type)
-            
+                self.find_or_create_instance(
+                    instance_name_parts[1],
+                    menu_instance_type,
+                    instance_line_type
+                    )
+
             elif instance_type_name == "code":
                 # TODO: Try to integrate this part inside find_or_create_instance function
                 code_instance = Instance.objects.filter(name=instance_name_parts[0],
-                                                            instance_type=code_instance_type,
-                                                            project=self.block.file.project)
-                
+                                                        instance_type=code_instance_type,
+                                                        project=self.block.file.project)
+
                 if len(code_instance) == 1:
                     code_instance = code_instance[0]
                 else:
                     code_instance = Instance.objects.create(
                         name=instance_name_parts[0],
-                        instance_type= code_instance_type,
+                        instance_type=code_instance_type,
                         project=self.block.file.project
                     )
 
@@ -182,11 +219,11 @@ class TranslationLine(models.Model):
                 else:
                     code_pos_instance = Instance.objects.create(
                         name=instance_name_parts[1],
-                        instance_type= code_pos_instance_type,
+                        instance_type=code_pos_instance_type,
                         project=self.block.file.project,
                         parent=code_instance
                     )
-                  
+
                 self.instance = code_pos_instance
                 self.line_type = instance_line_type
         else:
@@ -200,11 +237,9 @@ class TranslationLine(models.Model):
                 )
             self.instance = other_instance
             self.line_type = other_line_type
-                
 
-        
         self.save()
-    
+
     class Meta:
         # rename table created by django in db
-        db_table="translation_line"
+        db_table = "translation_line"
